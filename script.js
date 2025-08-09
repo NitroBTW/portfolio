793e40d98682b5e1c04c258510fec9f04cf6acc2
NitroBTW <69312385+NitroBTW@users.noreply.github.com>
2025-08-09 02:02:59 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Animate elements on scroll
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show");
+                }
+            });
+        },
+        { threshold: 0.2 }
+    );
+
+    document.querySelectorAll(".hidden").forEach((el) => observer.observe(el))
+})
